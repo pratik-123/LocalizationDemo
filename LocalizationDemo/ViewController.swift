067f20f43b4ee78ea42fe78648061938c92417e4
Pratik <pratik.lad@outlook.com>
2021-09-18 18:24:16 +0530

    PluralRule adding

--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -11,12 +11,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelPhone: UILabel!
     
     @IBOutlet weak var imageViewFlag: UIImageView!
+    
+    @IBOutlet weak var labelPen: UILabel!
+    
+    @IBOutlet weak var labelAmount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         labelPhone.text = NSLocalizedString("phonename", comment: "")
         imageViewFlag.image = UIImage(named: NSLocalizedString("flag", comment: "image name"))
+        
+        //I have 10 pens.
+        let qty = 05
+        
+        let str1 = String.localizedStringWithFormat(NSLocalizedString("pendata", comment: ""), qty)
+        
+        labelPen.text = str1
+        
+        
+        let nf = NumberFormatter.localizedString(from: 10000, number: .currency)
+        
+        let str2 = String.localizedStringWithFormat(NSLocalizedString("amount", comment: ""), nf)
+        
+        labelAmount.text = str2
+        
+        
     }
 
 
